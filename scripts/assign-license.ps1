513c2ee931bef020abf754dab1cf16eb9fbbd37c
techodyssey89 <139013476+techodyssey89@users.noreply.github.com>
2026-02-23 16:36:23 -0500

    Update assign-license.ps1

--- scripts/assign-license.ps1
+++ scripts/assign-license.ps1
@@ -1,1 +1,17 @@
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$UserPrincipalName,
 
+    [Parameter(Mandatory=$true)]
+    [string]$SkuId
+)
+
+Connect-MgGraph -Scopes "User.ReadWrite.All"
+
+# Get the user
+$user = Get-MgUser -UserId $UserPrincipalName
+
+# Assign the license
+Set-MgUserLicense -UserId $user.Id `
+    -AddLicenses @{SkuId = $SkuId} `
+    -RemoveLicenses @()
