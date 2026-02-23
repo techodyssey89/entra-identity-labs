db38a1e03b36af3d898bb9f421545f458508de2b
techodyssey89 <139013476+techodyssey89@users.noreply.github.com>
2026-02-23 16:33:34 -0500

    Update create-users.ps1

--- scripts/create-users.ps1
+++ scripts/create-users.ps1
@@ -1,1 +1,7 @@
+Connect-MgGraph -Scopes "User.ReadWrite.All"
 
+New-MgUser -DisplayName "Adele Vance" `
+  -UserPrincipalName "AdeleV@yourtenant.onmicrosoft.com" `
+  -MailNickname "AdeleV" `
+  -PasswordProfile @{ Password = "P@ssw0rd123!" } `
+  -AccountEnabled $true
